73e385bbcd7d142e811c7f13e8c5530c86e9810c
Aliosseily <ali.ossaily@outlook.com>
2018-09-23 20:00:51 +0300

    bitmal

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("test")
+
+var button = document.querySelector('.btnUpdate');
+button.onclick = function(){
+    document.querySelector('.btnUpdate').className = "hideText";
+    document.getElementById('Text').className = 'show-Text-button';
+    document.getElementById('Button').className = 'show-Text-button';
+    
+}
